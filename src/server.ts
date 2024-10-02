8c5c91f24cb3cb640f8f046f21d341554bd6d08b
Azutech <azunna.onugha@gmail.com>
2024-10-02 04:55:22 +0100

    Chore () : Ticketer server created for Use

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import express, { Application, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { config } from 'dotenv';
+
+config();
+
+import { PORT } from './utils/config';
+
+
+const server: Application = express();
+
+
+
+server.use(express.json());
+//
+// server.use('/api/v1', routes);
+server.use(express.urlencoded({ extended: true }));
+
+server.get('/', (req: Request, res: Response) => {
+	res.status(StatusCodes.OK).json({ msg: 'Welcome To Mac-Ticketer 🎫🎫' });
+});
+
+server.get('*', (req: Request, res: Response) => {
+	res.status(StatusCodes.NOT_FOUND).json({ message: 'route not found 🔎' });
+});
+
+server.listen(PORT, () => {
+	console.log(`Mac-Ticketer is listening at http://localhost:${PORT} 🚀🚀`);
+});
+
+
+export default { server };
